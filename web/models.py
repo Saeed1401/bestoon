9518c4919ed8fffa5e5da129291c4615b9708e59
Saeed Eslampour <eslampoursaeed278@gmail.com>
2023-03-08 15:42:32 +0330

    Add Income to models and some beautification

--- web/models.py
+++ web/models.py
@@ -7,7 +7,19 @@ class Expense(models.Model):
 	text = models.CharField(max_length=255)
 	date = models.DateTimeField()
 	amount = models.BigIntegerField()
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
+	def __str__(self):
+		return f"{self.date}-{self.amount}"
+
+
+class Income(models.Model):
+	text = models.CharField(max_length=255)
+	date = models.DateTimeField()
+	amount = models.BigIntegerField()
+	user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+	def __str__(self):
+		return f"{self.date}-{self.amount}"
 
 
